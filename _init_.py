2e7e6fa52ee060316a0543dbcf54be9e6e8cea46
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-02 01:20:13 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -14,7 +14,28 @@ import plotly.graph_objs as go
 import plotly
 from flask import url_for
 
-app=Flask(__name__, static_url_path='/static')
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
 
 
 @app.route('/')
