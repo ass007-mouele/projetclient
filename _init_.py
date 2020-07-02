d057f598790cdcc1f0dcc25c4b79069940b33073
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-02 11:44:20 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -8,26 +8,7 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from flask import url_for
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+app=Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
@@ -103,7 +84,8 @@ def prediction():
 	#print("les valeurs de prediction sont:\n",modelLR.predict(X[-4:-1]))
 
 
-
+if __name__=='__main__':
+	app.run(debug=True,port=3000)
 
 
 
