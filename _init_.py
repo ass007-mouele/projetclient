a1207ed2685440614739bd27c43e7450e86c7b6d
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-02 13:23:19 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -11,12 +11,9 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from flask import url_for
 
+app = Flask(__name__)
 
- 
-from flask import  Blueprint
-api = Blueprint('api', __name__)
-
-@api.route("/")	
+@app.route("/")	
 def home():
 	#con = sqlite3.connect("base_h2eau.db")
 	#dframe = pd.read_sql_query("SELECT * FROM H2eau", con)
@@ -26,12 +23,12 @@ def home():
 	return render_template ('pages/home.html')
 
 
-@api.route('/addmesures')
+@app.route('/addmesures')
 def mesures():
 	return render_template ('pages/addmesures.html')
 
 
-@api.route('/addrec',methods = ['POST', 'GET'])
+@app.route('/addrec',methods = ['POST', 'GET'])
 def addrec(): 
 	if request.method == 'POST':
 		try:
@@ -64,7 +61,7 @@ def addrec():
 	#return render_template ('pages/donnees.html')
 
 
-@api.route('/donnees')
+@app.route('/donnees')
 def donnees():
 	con = sqlite3.connect("base_h2eau.db")
 	con.row_factory = sqlite3.Row
@@ -73,7 +70,7 @@ def donnees():
 	rows = cur.fetchall();
 	return render_template("pages/donnees.html",rows = rows)
 
-@api.route('/predict',methods = ['POST', 'GET'])
+@app.route('/predict',methods = ['POST', 'GET'])
 def prediction():
 	con = sqlite3.connect("base_h2eau.db")
 	df = pd.read_sql_query("SELECT * FROM H2eau", con)
@@ -96,8 +93,8 @@ def create_app(config_file=None):
     app.register_blueprint(api)
     return app	
 
-#if __name__=='__main__':
-	#app.run(debug=True,port=3000)
+if __name__=='__main__':
+	app.run(debug=True,port=3000)
 
 
 
