2d2ea6821586ab53bb8339d67b4763130c55950b
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-02 12:14:24 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,6 +1,9 @@
 from flask import Flask , render_template , request
 import sqlite3
 #import pyodbc
+
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
