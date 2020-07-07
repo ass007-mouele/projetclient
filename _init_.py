11424161af72da157b3a2419febc5c55199484a6
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-08 00:25:35 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -16,10 +16,8 @@ import os
 
 app=Flask(__name__, static_url_path='/static')
 
-#app.config['DATABASE_URL'] = 'sqlite:///DataBase.sqlite3'
-app.config['DATABASE_URL'] = 'postgres://zcepddlyyfzmud:f727e3fe19092e4cfe714369694ce462c0eb655c229bb9007d6ce37ea54cee46@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d3gmstuaidusif'
+app.config['DATABASE_URL'] = 'sqlite:///DataBase.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-SECRET_KEY='dhdjshdjhddskdoskdoieiejennencec'
 db = SQLAlchemy(app)
 
 
