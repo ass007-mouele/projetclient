c2ee012711b87d73c432f15b8e36009622670281
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-08 01:03:37 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -18,8 +18,6 @@ import os
 
 app=Flask(__name__, static_url_path='/static')
 
-SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-
 app.config['DATABASE_URL'] = 'sqlite:///DataBase.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
