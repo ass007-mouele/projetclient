8a0472a808891db5306ea5facf55890eb536126e
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-09 19:57:52 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -24,7 +24,7 @@ app=Flask(__name__, static_url_path='/static')
 
 app.config['DEBUG'] = False
 app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL')
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY']=os.environ.get('KEY')
 db = SQLAlchemy(app)
 
 
