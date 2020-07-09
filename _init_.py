4318e628fbeb5fafebc110bbee2eff896fe60a49
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-09 17:07:41 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -19,34 +19,46 @@ import os
 app=Flask(__name__, static_url_path='/static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.sqlite3'
+app.secret_key='ffgggfgfgggfgfggfgfSECRET'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 
 class Post(db.Model):
 	__tablename__='Posts'
 	id = db.Column(db.Integer, primary_key=True)
-	Date = db.Column(db.String(80))
-	Heure = db.Column(db.Time)
-	Bassin = db.Column(db.String(80))
-	Transparence = db.Column(db.String(80))
-	Temperature_de_l_eau = db.Column(db.Float)
-	pH = db.Column(db.Float)
-	DPD_1 = db.Column(db.Float)
-	DPD_3 = db.Column(db.Float)
-	combine = db.Column(db.Text)
-	libre_actif = db.Column(db.Text)
+	Date = db.Column(db.String(80),nullable=False)
+	Heure = db.Column(db.String(80),nullable=False)
+	Bassin = db.Column(db.String(80),nullable=False)
+	Transparence = db.Column(db.String(80),nullable=False)
+	Temperature_de_l_eau = db.Column(db.String(80),nullable=False)
+	pH = db.Column(db.String(80),nullable=False)
+	DPD_1 = db.Column(db.String(80),nullable=False)
+	DPD_3 = db.Column(db.String(80),nullable=False)
+	combine = db.Column(db.String(80),nullable=False)
+	libre_actif = db.Column(db.String(80),nullable=False)
 	compteur = db.Column(db.Integer)
-	def __repr__(self):
- 		return '<Post "{}">'.format(self.Date)
+	
+	def __init__(self,Date,Heure,Bassin,Transparence,Temperature_de_l_eau,pH,DPD_1,DPD_3,combine,libre_actif,compteur):
+		self.Date = Date
+		self.Heure = Heure
+		self.Bassin = Bassin
+		self.Transparence = Transparence
+		self.Temperature_de_l_eau = Temperature_de_l_eau
+		self.pH= pH
+		self.DPD_1 = DPD_1
+		self.DPD_3 = DPD_3
+		self.combine = combine
+		self.libre_actif = libre_actif
+		self.compteur = compteur
+	def __repr__(self,id):
+ 		#return '<Post"{}{}{}{}{}{}{}{}{}{}{}">'.format(self.Date,self.Heure,self.Bassin,self.Transparence,self.Temperature_de_l_eau,self.pH,self.DPD_1,self.DPD_3,self.combine,self.libre_actif,self.compteur)
+ 		return '<Post"{}">'.format(self.id)
 
 
 @app.route('/')
 def home():
-	#con = sqlite3.connect("base_h2eau.db")
-	#dframe = pd.read_sql_query("SELECT * FROM H2eau", con)
-	#graph=px.bar(dframe, x=dframe["Date"][-30:], y=dframe["Combine"][-30:], title='Evolution du chlore combiné sur les 30 jours derniers')
-	#graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
 	return render_template ('pages/home.html')
 
 
@@ -55,39 +67,31 @@ def mesures():
 	return render_template ('pages/addmesures.html')
 
 
-@app.route('/addrec',methods = ['POST','GET'])
-def addrec(): 
+@app.route('/addmesures',methods = ['POST','GET'])
+def addmesures(): 
 	if request.method == 'POST':
-		try:
-			Date = request.form['Date']
-			Heure = request.form['Heure']
-			Bassin = request.form['Bassin']
-			Transparence = request.form['Transparence']
-			Temperature_de_l_eau = request.form['Temperature_de_l_eau']
-			pH = request.form['pH']
-			DPD_1 = request.form['DPD_1']
-			DPD_3 = request.form['DPD_3']
-			combine = request.form['combine']
-			libre_actif = request.form['libre_actif']
-			compteur = request.form['compteur']
-			p=Post(Date, Heure,Bassin,Transparence,Temperature_de_l_eau,pH,DPD_1,DPD_3,combine,libre_actif,compteur)
-			db.session.add(p)
-			db.session.commit()
-		except:
-			db.session.rollback()
-			msg = "erreur insertion "
-		finally:
-			db.session.close()
-			#msg = [Date,Heure,Bassin,Transparence,Temperature_de_l_eau,pH,DPD_1,DPD_3,combine,libre_actif,compteur]
-			return render_template("pages/donnees.html")#, msg = msg)
-			
-		
-
+	   Date = request.form.get('Date')
+	   Heure = request.form.get('Heure')
+	   Bassin = request.form.get('Bassin')
+	   Transparence = request.form.get('Transparence')
+	   Temperature_de_l_eau = request.form.get('Temperature_de_l_eau')
+	   pH = request.form.get('pH')
+	   DPD_1 = request.form.get('DPD_1')
+	   DPD_3 = request.form.get('DPD_3')
+	   combine = request.form.get('combine')
+	   libre_actif = request.form.get('libre_actif')
+	   compteur = request.form.get('compteur')
+	   p=Post(Date=Date,Heure=Heure,Bassin=Bassin,Transparence=Transparence,Temperature_de_l_eau=Temperature_de_l_eau,pH=pH,DPD_1=DPD_1,DPD_3=DPD_3,combine=combine,libre_actif=libre_actif,compteur=compteur)
+	   db.session.add(p)
+	   db.session.commit()
+	   flash("Mesures was successfully created")
+	   db.session.close()
+	   return render_template("pages/addmesures.html")	
 
 @app.route('/donnees')
 def donnees():
-	Posts=Post.query.all()
-	return render_template("pages/donnees.html",Post = Posts)
+	posts=Post.query.all()
+	return render_template("pages/donnees.html", Posts=Post.query.order_by(Post.id.desc()).all())
 
 
 @app.route('/predict',methods = ['POST', 'GET'])
@@ -97,20 +101,12 @@ def prediction():
 	X = df[['DPD_1','DPD_3']] 
 	y = df['Combine']
 	modelLR = LinearRegression().fit(X, y)
-	#if request.methods=='POST':
-		#comment=request.form['comment']
-		#data=[comment]
 	my_prediction=modelLR.predict(X[-4:-1])
 	return render_template("pages/predict.html", prediction = str(my_prediction))
 
 
 
-
-
-
-
-
-
-
-
+if __name__=='__main__':
+   db.create_all()
+   app.run(debug=True, port=3000)
 
