05aaa3bf23cf3ba5f51b53decc78201beb48e493
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-12 12:56:18 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template ,request,flash
+from flask import Flask ,Blueprint, render_template ,request,flash
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 #import pyodbc
@@ -94,9 +94,9 @@ def addmesures():
 		compteur = request.form.get('compteur')
 		p=Post(Date=Date,Heure=Heure,Bassin=Bassin,Transparence=Transparence,Temperature_de_l_eau=Temperature_de_l_eau,pH=pH,DPD_1=DPD_1,DPD_3=DPD_3,combine=combine,libre_actif=libre_actif,compteur=compteur)
 		db.session.add(p)
-		db.session.commit()
-		flash("Les mésures ont été enregistrées!!!!", 'success')	
+		db.session.commit()	
 		db.session.close()
+		flash("Les mésures ont été enregistrées!!!!", 'success')
 		return render_template("pages/addmesures.html")	
 	     
 
