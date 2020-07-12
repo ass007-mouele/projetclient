64434ec356305e641451a5005ec4acae41e69816
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-12 13:56:34 +0200

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,4 +1,4 @@
-from flask import Flask ,Blueprint, render_template ,request,flash
+from flask import Flask ,Blueprint, render_template ,request,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 #import pyodbc
@@ -95,10 +95,10 @@ def addmesures():
 		p=Post(Date=Date,Heure=Heure,Bassin=Bassin,Transparence=Transparence,Temperature_de_l_eau=Temperature_de_l_eau,pH=pH,DPD_1=DPD_1,DPD_3=DPD_3,combine=combine,libre_actif=libre_actif,compteur=compteur)
 		db.session.add(p)
 		db.session.commit()
-		flash("Les mésures ont été enregistrées!!!!")
 		db.session.close()
-		return render_template("pages/addmesures.html")	
-	return render_template("pages/addmesures.html")
+		flash("Les mésures ont été enregistrées!!!!")
+		return redirect(url_for("addmesures"))	
+	#return render_template("pages/addmesures.html")
 
 @app.route('/donnees')
 def donnees():
