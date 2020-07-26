7d1d9159580d7fd96327e2b3f8c700fdd83f6cea
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 18:35:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key =os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
