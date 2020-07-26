3caf549d322a5ff32528ece2b7317275e3660e4c
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 15:26:59 +0200

    Update db_create.py

--- db_create.py
+++ db_create.py
@@ -5,5 +5,4 @@ db.create_all()
 
 #p=Post(Date="2018-08-07",Heure=" 06:00",Bassin="GB",Transparence="Clair",Temperature_de_l_eau=27,pH=7.5,DPD_1=1.7,DPD_3="0.4",combine=1.9,libre_actif=0.8,compteur="45678")
 #db.session.add(p)
-
-db.session.commit()
+#db.session.commit()
