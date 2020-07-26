ba93d8ede66283c91ed91e408a9f3c0bd58d3533
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 20:05:50 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager , Server
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 
@@ -7,7 +7,9 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+manager.run()
 
+#if __name__ == '__main__':
+    #manager.run()
 
-if __name__ == '__main__':
-    manager.run()
+#manager.add_command('runserver', Server(host='localhost', port=8080, debug=True))
