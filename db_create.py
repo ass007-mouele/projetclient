7df61770712478fe359c3e01968bf4aad216ce1f
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 12:49:47 +0200

    Add files via upload

---
+++ db_create.py
@@ -0,0 +1,9 @@
+from app import Post, db
+
+
+db.create_all()
+
+p=Post(Date="2018-08-07",Heure=" 06:00",Bassin="GB",Transparence="Clair",Temperature_de_l_eau=27,pH=7.5,DPD_1=1.7,DPD_3="0.4",combine=1.9,libre_actif=0.8,compteur="45678")
+db.session.add(p)
+
+db.session.commit()
