9416e089da432c4734ec4d84aadda201268191e4
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 21:07:52 +0200

    Update db_create.py

--- db_create.py
+++ db_create.py
@@ -6,7 +6,7 @@ import os
 # Put your URL in an environment variable and connect.
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-db.create_all()
+#db.create_all()
 
 
 #p=Post(Date="2018-08-07",Heure=" 06:00",Bassin="GB",Transparence="Clair",Temperature_de_l_eau=27,pH=7.5,DPD_1=1.7,DPD_3="0.4",combine=1.9,libre_actif=0.8,compteur="45678")
