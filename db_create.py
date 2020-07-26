53173d2948072f11f7dae6e07dd5cc2587443836
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-26 16:44:20 +0200

    Update db_create.py

--- db_create.py
+++ db_create.py
@@ -1,7 +1,7 @@
 from app import Post, db
 
-
 db.create_all()
+session.close()
 
 
 #p=Post(Date="2018-08-07",Heure=" 06:00",Bassin="GB",Transparence="Clair",Temperature_de_l_eau=27,pH=7.5,DPD_1=1.7,DPD_3="0.4",combine=1.9,libre_actif=0.8,compteur="45678")
