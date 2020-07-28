d6fae92c43086f034e0138aff3ac7263c5af32e0
assoni Mouele <60751252+ass007-mouele@users.noreply.github.com>
2020-07-28 22:47:48 +0200

    Update modelprediction.py

--- modelprediction.py
+++ modelprediction.py
@@ -19,7 +19,6 @@ df_pool = pd.read_sql(sql = db.session.query(Post)\
 df_pool['Date'] = pd.to_datetime(df_pool['Date'])
 
 
-
 ##################################################################################################################################################################
 
 #8.Prédictions et calcul du Chlore combiné et libre actif
@@ -43,6 +42,7 @@ def find_date(last_date, new_date):
 
 # on crée une variable retournant la dernière date du jeu de données en notre possession
 last_date = df_pool["Date"].iloc[-1]
+last_date = pd.to_datetime(last_date, dayfirst=True)
 
 # on crée une boucle pour générer les dates de prédictions et pour les ajouter à notre dataframe
 for number in range(14):
@@ -54,8 +54,9 @@ for number in range(14):
 # on affiche les dates comme étant les index du dataframe
 df_lstm.set_index(keys="Date", drop=True, inplace=True)
 
+# on crée un autre dataframe (une copie du dataframe plus haut) pour stocker les probabilités de nos prédictions
+df_proba = df_lstm.copy()
 
-#Chargement des modèles et calcul des prédictions
 
 # pour charger et afficher les éléments clés de nos modèles de prédiction, on importe les modules suivants
 from keras.models import load_model
@@ -65,6 +66,7 @@ regressor_temp = load_model("lstm_temp.h5")
 regressor_pH = load_model("lstm_pH.h5")
 regressor_DPD1 = load_model("lstm_DPD1.h5")
 regressor_DPD3 = load_model("lstm_DPD3.h5")
+
 
 # avant de faire nos prédictions, on sélectionne les données que l'on souhaite prédire (les 60 dernières observations de chacune des 4 variables indépendantes)
 predicting = df_pool.iloc[-60:][["Temperature_de_l_eau", "pH", "DPD_1", "DPD_3"]]
@@ -88,27 +90,33 @@ for column in predicting:
 # on crée une boucle, qui en fonction des données d'entrée, va utiliser l'un des modèles chargés précédemment et prédire des valeurs pour les dernières 14 demi-journées
   if column == "Temperature_de_l_eau":
     predicted_values = regressor_temp.predict(predict_set_scaled)
+    proba_prediction = regressor_temp.predict_proba(predict_set_scaled)
   if column == "pH":
     predicted_values = regressor_pH.predict(predict_set_scaled)
+    proba_prediction = regressor_pH.predict_proba(predict_set_scaled)
   if column == "DPD_1":
     predicted_values = regressor_DPD1.predict(predict_set_scaled)
+    proba_prediction = regressor_DPD1.predict_proba(predict_set_scaled)
   if column == "DPD_3":
     predicted_values = regressor_DPD3.predict(predict_set_scaled)
+    proba_prediction = regressor_DPD3.predict_proba(predict_set_scaled)
 
 # on supprime la mise à l'échelle des données prédites
   predicted_values = scaler.inverse_transform(predicted_values)
 
 # on remet les données prédites au bon format
   predicted_values = np.reshape(predicted_values, (predicted_values.shape[1], predicted_values.shape[0]))
-
-# on ajoute ces valeurs au dataframe crée plus tôt
-
+  proba_prediction = np.reshape(proba_prediction, (proba_prediction.shape[1], proba_prediction.shape[0]))
+  
+# on ajoute les valeurs prédites au dataframe crée plus tôt
   df_lstm[column] = predicted_values
+  df_proba[column] = proba_prediction
 
-  #Calcul des valeurs de Chlore combiné et libre actif
+  #Calcul des valeurs predictes de Chlore combiné et libre actif (et probabilités)
 
   #Rappel des fonctions de calculs du Chlore combiné et libre actif ci-dessous
 
+# Pour le chlore combiné, utilisez la fonction suivante
 # Pour le chlore combiné, utilisez la fonction suivante
 def chlorecomb_finder(DPD_1, DPD_3):
   result = DPD_3 - DPD_1
@@ -138,7 +146,11 @@ df_lstm["Combine"] = chlorecomb_finder(df_lstm["DPD_1"], df_lstm["DPD_3"])
 # on utilise la fonction libreactif_finder utilisée pour le calcul automatique du Chlore libre actif
 df_lstm["Libre_Actif"] = libreactif_finder(df_lstm["Temperature_de_l_eau"], df_lstm["pH"], df_lstm["DPD_1"])
 
-# on affiche notre nouveau dataframe ainsi que sa taille
-#print(df_lstm.shape)
-#print('ALL GOOD')
-#print(df_lstm)
+# on calcule également les probabilités des valeurs prédites pour le chlore combiné... 
+df_proba["Combine"] = (df_proba["DPD_1"] + df_proba["DPD_3"]) / 2
+
+# ...et pour le chlore libre actif
+df_proba["Libre_Actif"] = (df_proba["Temperature_de_l_eau"] + df_proba["pH"] + df_proba["DPD_1"]) / 3
+
+# on multiplie le tout par 100 pour avoir des pourcentages
+df_proba = df_proba * 100
